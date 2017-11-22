c053add3ede300c58f38446ee37f6ea1720c6c9b
ZhenyaTurkina <31654808+ZhenyaTurkina@users.noreply.github.com>
2017-11-22 21:52:04 +0300

    Add files via upload

--- hw4/hw4.py
+++ hw4/hw4.py
@@ -1,1 +1,28 @@
+with open("txt.txt", encoding = 'utf-8') as f:
+    text = f.read() 
+text = text.replace(",", "")
+text = text.replace(".", "")
+lol = text.split()
+print(lol)
 
+a = len(lol)
+
+#print (a)
+
+k = 0
+s = 0
+
+for i in range (0,a):
+    if lol[i].isalpha():
+        m = len(lol[i])
+        if m == 3:
+            k+=1
+            #print (k)
+        elif m == 1:
+            s+=1
+            #print (s)
+        if m == 0:
+            print ("Однобуквенных слов не существует")
+        
+print ("3-буквенных слов больше 1-буквенных в", k/s, "раз")
+    
