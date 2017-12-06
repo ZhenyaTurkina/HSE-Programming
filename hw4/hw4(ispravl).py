8853ff78ab2d1a5990888613acefd297aed0f976
ZhenyaTurkina <31654808+ZhenyaTurkina@users.noreply.github.com>
2017-12-06 23:43:06 +0300

    Create hw4(ispravl).py

---
+++ hw4/hw4(ispravl).py
@@ -0,0 +1,27 @@
+with open("kryak.txt", encoding = 'utf-8') as f:
+    text = f.read() 
+text = text.replace(",", "")
+text = text.replace(".", "")
+lol = text.split()
+print(lol)
+
+a = len(lol)
+
+print (a)
+
+k = 0
+s = 0
+
+for i in range (0,a):
+    if lol[i].isalpha():
+        m = len(lol[i])
+        if m == 3:
+            k+=1
+            #print (k)
+        elif m == 1:
+            s+=1
+    
+if s==0:
+    print('Однобуквенных слов не существует')
+else:
+    print ("3-буквенных слов больше 1-буквенных в", k/s, "раз")
